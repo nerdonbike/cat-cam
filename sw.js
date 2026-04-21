c7744997280f8f959b4e1b04575148ad483da185
nerdonbike <filip.ruckh@gmail.com>
2026-04-21 08:44:52 +0200

    Refactor service worker for better caching

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2869
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+const CACHE = 'katzen-monitor-v2';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
-\f0\fs24 \cf0 const CACHE = 'katzen-monitor-pwa-v2';\
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];\
-self.addEventListener('install', event => \{\
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));\
-  self.skipWaiting();\
-\});\
-self.addEventListener('activate', event => \{\
-  event.waitUntil(self.clients.claim());\
-\});\
-self.addEventListener('fetch', event => \{\
-  if (event.request.method !== 'GET') return;\
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));\
-\});}
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
